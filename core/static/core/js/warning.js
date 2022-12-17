6ef395b972b3a473f05f76196eeb4c5bd3b1e949
Om Dahale <om.g.dahale@gmail.com>
2022-12-17 23:44:04 +0530

    add full screen

--- core/static/core/js/warning.js
+++ core/static/core/js/warning.js
@@ -12,10 +12,30 @@ jQuery_3_6_0(document).ready(function(){
         blockWarning = true;
     });
 
-    jQuery_3_6_0(document).mouseenter(function (e) {
-        if (!blockWarning){
-            $('.modal').modal('hide');
-            $('#modal-window-warning').modal('show');
+    function requestFullScreen() {
+        // Supports most browsers and their versions.
+        var element = document.body; // Make the body go full screen.
+        var requestMethod = element.requestFullScreen || element.webkitRequestFullScreen || element.mozRequestFullScreen || element.msRequestFullScreen;
+
+        if (requestMethod) { // Native full screen.
+            requestMethod.call(element);
+        } else if (typeof window.ActiveXObject !== "undefined") { // Older IE.
+            var wscript = new ActiveXObject("WScript.Shell");
+            if (wscript !== null) {
+                wscript.SendKeys("{F11}");
+            }
         }
-    });
+
+        jQuery_3_6_0("#js-fullscreen-splash").addClass("d-none");
+        jQuery_3_6_0("#js-main").removeClass("d-none");
+
+        jQuery_3_6_0(document).mouseenter(function (e) {
+            if (!blockWarning){
+                $('.modal').modal('hide');
+                $('#modal-window-warning').modal('show');
+            }
+        });
+    }
+
+    jQuery_3_6_0("#js-fullscreen-prompt").click(requestFullScreen);
 });
