025bfefc8239301042cbddac512ac0a6432d2ea2
Om Dahale <om.g.dahale@gmail.com>
2022-12-24 19:29:32 +0530

    Show warning on fullscreen exit

--- core/static/core/js/warning.js
+++ core/static/core/js/warning.js
@@ -2,14 +2,15 @@ jQuery_3_6_0(document).ready(function(){
     // When alt+tab mouseenter event fires twice,
     // when leaving the window and when coming back again,
     // therefore we can block the warning on blur and turn it back on again on focus
-    let blockWarning = false;
+    let windowWarningStarted = false;
+    let blockWindowWarning = false;
 
     jQuery_3_6_0(window).on("focus", function() {
-        blockWarning = false;
+        blockWindowWarning = false;
     });
 
     jQuery_3_6_0(window).on("blur", function() {
-        blockWarning = true;
+        blockWindowWarning = true;
     });
 
     function requestFullScreen() {
@@ -25,17 +26,29 @@ jQuery_3_6_0(document).ready(function(){
                 wscript.SendKeys("{F11}");
             }
         }
-
-        jQuery_3_6_0("#js-fullscreen-splash").addClass("d-none");
-        jQuery_3_6_0("#js-main").removeClass("d-none");
-
-        jQuery_3_6_0(document).mouseenter(function (e) {
-            if (!blockWarning){
-                $('.modal').modal('hide');
-                $('#modal-window-warning').modal('show');
-            }
-        });
     }
 
+    const query = matchMedia('(display-mode: fullscreen)')
+    query.addListener(e => {
+        if (!e.matches){ // fullscreen exit
+            jQuery_3_6_0("#js-main").addClass("d-none");
+            jQuery_3_6_0("#js-fullscreen-splash").removeClass("d-none");
+            $('.modal').modal('hide');
+            $('#modal-fullscreen-warning').modal('show');
+        }
+        else { // fullscreen enter
+            if (!windowWarningStarted){
+                jQuery_3_6_0(document).mouseenter(function (e) {
+                    if (!blockWindowWarning){
+                        $('.modal').modal('hide');
+                        $('#modal-window-warning').modal('show');
+                    }
+                });
+                windowWarningStarted = true;
+            }
+            jQuery_3_6_0("#js-fullscreen-splash").addClass("d-none");
+            jQuery_3_6_0("#js-main").removeClass("d-none");
+        }
+    });
     jQuery_3_6_0("#js-fullscreen-prompt").click(requestFullScreen);
 });
